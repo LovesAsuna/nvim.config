39af74817e804727dd52af5262d83fa70c415055
LovesAsuna <qq625924077@gmail.com>
2024-01-01 23:28:01 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -43,4 +43,7 @@ require("lazy").setup({
       },
     },
   },
+  git = {
+    url_format = "https://mirror.ghproxy.com//https://github.com/%s.git",
+  },
 })
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        kotlin_language_server = {},
+      },
+    },
+  },
+}
